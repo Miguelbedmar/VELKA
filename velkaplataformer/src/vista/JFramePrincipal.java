09e7e44f1176553ac16e71791c1344265bb1e19f
Miguel <miguelbedmards@alumnos.ilerna.com>
2026-03-03 19:44:41 +0100

    Se ha añadido  un icono pendiente de correccion

--- velkaplataformer/src/vista/JFramePrincipal.java
+++ velkaplataformer/src/vista/JFramePrincipal.java
@@ -1,17 +1,20 @@
 package vista;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class JFramePrincipal extends JFrame {
 
 	public JFramePrincipal() {
-		setTitle("VELKA"); //Cambiamos el titulo de la pestaña como VELKA
-		setSize(700 ,500); // Indicamos que  anchura y altura tendra
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Permite parar el programa cerrando la ventana  
-        setLocationRelativeTo(null);
-        setResizable(false); // Eliminamos la posibilidad de que el usuario redimensione la ventana
-        setVisible(true);
+		setTitle("VELKA"); // Cambiamos el titulo de la pestaña como VELKA
+		setSize(700, 500); // Indicamos que anchura y altura tendra
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Permite parar el programa cerrando la ventana
+		setLocationRelativeTo(null);
+		setResizable(false); // Eliminamos la posibilidad de que el usuario redimensione la ventana
+		setVisible(true);
+		
+		ImageIcon icono = new ImageIcon(getClass().getResource("/recursos/VELKAICONO.png"));
+		setIconImage(icono.getImage());
 	}
-	
 
 }
