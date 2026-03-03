b8ca58a97293154597b312966502b9306a8370b5
Miguel <miguelbedmards@alumnos.ilerna.com>
2026-03-03 19:13:11 +0100

    Se ha corregido el diagrama de clase

--- velkaplataformer/src/vista/JFramePrincipal.java
+++ velkaplataformer/src/vista/JFramePrincipal.java
@@ -5,11 +5,11 @@ import javax.swing.JFrame;
 public class JFramePrincipal extends JFrame {
 
 	public JFramePrincipal() {
-		setTitle("VELKA");
-		setSize(700 ,500);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("VELKA"); //Cambiamos el titulo de la pestaña como VELKA
+		setSize(700 ,500); // Indicamos que  anchura y altura tendra
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Permite parar el programa cerrando la ventana  
         setLocationRelativeTo(null);
-        setResizable(false);
+        setResizable(false); // Eliminamos la posibilidad de que el usuario redimensione la ventana
         setVisible(true);
 	}
 	
