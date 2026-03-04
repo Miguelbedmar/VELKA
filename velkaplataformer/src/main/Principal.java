a70ec3053bcd78ae3ed18c07f6613dd86afd14e8
Miguel <miguelbedmards@alumnos.ilerna.com>
2026-03-04 19:30:01 +0100

    Se ha comenzado a trabajar con el menu

--- velkaplataformer/src/main/Principal.java
+++ velkaplataformer/src/main/Principal.java
@@ -1,7 +1,7 @@
 package main;
 
 import vista.JFramePrincipal;
-
+import javax.swing.ImageIcon;
 public class Principal {
 	
 	public static void main(String[] args) {
--- velkaplataformer/src/vista/JPanelMenu.java
+++ velkaplataformer/src/vista/JPanelMenu.java
@@ -1,5 +1,23 @@
 package vista;
 
-public class JPanelMenu {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+
+public class JPanelMenu extends JPanel implements ActionListener {
+
+	 public JPanel fondo;
+	 public JPanel fondoBoton;
+	
+
+	private void configurarFondoMenu() {
+	
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//
+
+	}
 
 }
