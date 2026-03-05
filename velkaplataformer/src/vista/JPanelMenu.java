97817bea2c11396311dff72da0d5b1754009f81a
Miguel <miguelbedmards@gmail.com>
2026-03-05 12:14:03 +0100

    Se han añadido dos nuevas imanges al proyecto

--- velkaplataformer/src/vista/JPanelMenu.java
+++ velkaplataformer/src/vista/JPanelMenu.java
@@ -3,7 +3,6 @@ package vista;
 import java.awt.Dimension;
 import java.awt.Graphics;
 
-
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
@@ -11,12 +10,12 @@ public class JPanelMenu extends JPanel {
 	public Dimension dimension;
 	// public JPanel fondoBoton;
 
-	private void configurarFondoMenu(Graphics g) {
+	public  void configurarFondoMenu(Graphics g) {
 		dimension.getSize();
-		ImageIcon fondo = new ImageIcon(getClass().getResource("/recursos/VELKA.jpg"));
+		ImageIcon fondo = new ImageIcon(getClass().getResource("/recursos/FONDO-MENU.png"));
 		g.drawImage(fondo.getImage(), 0, 0, dimension.width, dimension.height, null);
 		setOpaque(true);
-		super.paint(g);
+		super.paintChildren(g);
 	}
 
 
