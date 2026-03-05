4d28d8c70cd991af2b19d5c35bf809901e6597fa
Miguel Bedmar <miguelbedmards@gmail.com>
2026-03-05 15:22:56 +0100

    falta colocar el fondo menu

--- velkaplataformer/src/vista/JFramePrincipal.java
+++ velkaplataformer/src/vista/JFramePrincipal.java
@@ -4,7 +4,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class JFramePrincipal extends JFrame {
-	JpanelMenu fondoMenu = new JpanelMenu();
 
 	public JFramePrincipal() {
 		setTitle("VELKA"); // Cambiamos el titulo de la pestaña como VELKA
@@ -12,11 +11,11 @@ public class JFramePrincipal extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Permite parar el programa cerrando la ventana
 		setLocationRelativeTo(null);
 		setResizable(false); // Eliminamos la posibilidad de que el usuario redimensione la ventana
-		setVisible(true);
-
+		
 		ImageIcon icono = new ImageIcon(getClass().getResource("/recursos/VELKAAA.png"));
 		setIconImage(icono.getImage());
-		add(fondoMenu);
+		add(new JpanelMenu());
+		setVisible(true);
 	}
 
 }
--- velkaplataformer/src/vista/JpanelMenu.java
+++ velkaplataformer/src/vista/JpanelMenu.java
@@ -1,21 +1,17 @@
 package vista;
 
 import java.awt.Dimension;
-import java.awt.Graphics;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
-public class JpanelMenu extends JPanel{
+public class JpanelMenu extends JPanel {
 
-    @Override
-    public void paint(Graphics g){
-    	Dimension dimension=new Dimension().getSize();
-    	ImageIcon fondo=new ImageIcon(getClass().getResource("/recursos/FONDOMENU.png"));
-        g.drawImage(fondo.getImage(), 0, 0, dimension.width, dimension.height, null);
-        setOpaque(false);
-       super.paint(g);
-    }
-	
-	
+	public JpanelMenu() {
+		Dimension dimension = new Dimension().getSize();
+		ImageIcon fondo = new ImageIcon(getClass().getResource("/recursos/FONDOMENU.png"));
+		
+	}
+
+
 }
