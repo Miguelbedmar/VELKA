7c7fea8eaf9338b5a79a11d33b9e89567f6645ad
Miguel Bedmar <miguelbedmards@gmail.com>
2026-03-09 20:57:25 +0100

    Se ha determinado un matriz bidimensional y un tamaño de 32

--- velkaplataformer/src/modelo/Map.java
+++ velkaplataformer/src/modelo/Map.java
@@ -3,24 +3,17 @@ package modelo;
 public class Map {
 
 	private int[][] Mapa;
-	
+	private int title = 32;
+
 	/*
 	 * Está clase se encarga de modelar el mapa por medio de matrices de 32*32 que
 	 * contendran cada parte del mapa el mapa tendrá colisiones y elementos en donde
 	 * el jugador pordrá escalar y mantenerse sobre la misma.
 	 */
 
-	// 1. Primera Matriz.
+	// Matrices.
+	public Map() {
 
-	public int[][] matricesMapa(int[][] mapa) {
-		
-		Mapa= {
-				
-		};
-		
-		
-		
-		return Mapa;
 	}
 
 }
