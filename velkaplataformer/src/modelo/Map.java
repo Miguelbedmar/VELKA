a08c2e3fafdfa93f6faba6bbba165fb2662e3871
Miguel Bedmar <miguelbedmards@gmail.com>
2026-03-09 19:51:44 +0100

    Fondo del juego y Elementos  temporales hechos con Banana2.0 añañadidos

--- velkaplataformer/src/modelo/Map.java
+++ velkaplataformer/src/modelo/Map.java
@@ -3,4 +3,9 @@ package modelo;
 public class Map {
 	
 	
+	/* Está clase se encarga de modelar el mapa por medio de matrices
+	 * de 32*32 que contendran cada parte del mapa 
+	 * el mapa tendrá colisiones y elementos en donde el jugador pordrá escalar y mantenerse
+	 * sobre la misma*/
+	
 }
