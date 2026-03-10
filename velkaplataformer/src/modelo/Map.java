0e96f377bf52f05a0a5e94b2d75001ff74696fdb
Miguel <miguelbedmards@gmail.com>
2026-03-10 10:37:19 +0100

    Se ha terminado la primera matriz del mapa

--- velkaplataformer/src/modelo/Map.java
+++ velkaplataformer/src/modelo/Map.java
@@ -2,8 +2,8 @@ package modelo;
 
 public class Map {
 
-	private int[][] Mapa;
-	private int title = 32;
+	private int[][] mapa;
+	private int titleSi = 32;
 
 	/*
 	 * Está clase se encarga de modelar el mapa por medio de matrices de 32*32 que
@@ -13,7 +13,35 @@ public class Map {
 
 	// Matrices.
 	public Map() {
+		mapa= new int [][]{
+	
+			
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},	
+	{0,1,1,1,0,0,0,0,0,0,0,0,0,0,0},
+	{2,0,0,0,0,1,0,1,1,0,0,0,0,0,1},
+	{0,0,1,0,0,0,0,0,0,2,2,2,1,0,0},	
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},	
+	{1,1,1,1,1,0,0,0,0,0,0,1,0,1,0},
 
+			
+		};	
+	
+	
+}
+
+	public int[][] getMapa() {
+		return mapa;
 	}
 
+	public void setMapa(int[][] mapa) {
+		this.mapa = mapa;
+	}
+
+	public int getTitleSi() {
+		return titleSi;
+	}
+
+	public void setTitleSi(int titleSi) {
+		this.titleSi = titleSi;
+	}
 }
