b704dfcc1cf77471ee47f4a83c089cd5f9703489
Miguel Bedmar <miguelbedmards@gmail.com>
2026-03-11 16:00:55 +0100

    Se ha reorganizado el comentario de la clase modelo Map

--- velkaplataformer/src/modelo/Map.java
+++ velkaplataformer/src/modelo/Map.java
@@ -10,10 +10,14 @@ public class Map {
 	 * Está clase se encarga de modelar el mapa por medio de matrices de 32*32 que
 	 * contendran cada parte del mapa el mapa tendrá colisiones y elementos en donde
 	 * el jugador pordrá escalar y mantenerse sobre la misma. La matriz tendra
-	 * número del 0 al 3 teniendo cada un significado 0: sin colisiones 1: colisión
-	 * y plataforma 2: colisión y enemigo que restara vida al jugador 3: Colisión y
-	 * coleccionable que el jugador pordrá recolectar. 4. Siguiente o anterior
-	 * matriz. 5. Fin del juego.
+	 * número del 0 al 5 teniendo cada un significado
+	 *  0: sin colisiones 
+	 * 1: colisióny plataforma 
+	 * 2: colisión y enemigo que restara vida al jugador 
+	 * 3: Colisión y coleccionable que el jugador pordrá recolectar. 
+	 * 4. Siguiente o anterior
+	 * matriz. 
+	 * 5. Fin del juego.
 	 */
 
 	// Matrices.
@@ -22,7 +26,8 @@ public class Map {
 		room = new int[][][] {
 
 				// Primera matriz principio de nivel
-				{ { 3, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+						{ 
+						{ 3, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 						{ 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 						{ 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 4 },
 						{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
@@ -63,7 +68,8 @@ public class Map {
 						{ 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 4 },
 						{ 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 },
 						{ 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 },
-						{ 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2 }
+						{ 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+						{ 0, 0, 0, 0, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2 }
 
 				},
 
