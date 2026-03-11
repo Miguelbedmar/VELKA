3d37ab7683a50c82f4b5ae7d737ca0eb9507bd65
Miguel <miguelbedmards@gmail.com>
2026-03-11 10:14:00 +0100

    Se ha explicado con un comentario largo que función tendra la clase tile

--- velkaplataformer/src/modelo/Tile.java
+++ velkaplataformer/src/modelo/Tile.java
@@ -1,5 +1,20 @@
 package modelo;
 
 public class Tile {
+	
+	/* Está clase dentro del paquete de los modelos se encargara de modelar la estructura del modelo mapa  mediante aspectos gráficos
+	 * para ello se utilizaran los distintos sprites  para las platoformas pinchos y coleccionables.
+	 * a su vez  de que para que está clase funcione de manera correcta se ha establecido  un atributo tridimensional en  la clase map
+	 * que contiene 7 matrices que acaparan el mapa.*/
+
+
+
+
+
+
+
+
+
+
 
 }
