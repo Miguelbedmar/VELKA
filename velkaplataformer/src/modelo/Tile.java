739ae9e9e2afe9002325c82765e66b616966468c
Miguel <miguelbedmards@gmail.com>
2026-03-11 10:21:34 +0100

    Se ha establecido los atributos constructor y getters setters de la clase Tile

--- velkaplataformer/src/modelo/Tile.java
+++ velkaplataformer/src/modelo/Tile.java
@@ -1,20 +1,52 @@
 package modelo;
 
+import java.awt.Image;
+
 public class Tile {
+
+	/*
+	 * Está clase dentro del paquete de los modelos se encargara de modelar la
+	 * estructura del modelo mapa mediante aspectos gráficos para ello se utilizaran
+	 * los distintos sprites para las platoformas pinchos y coleccionables. a su vez
+	 * de que para que está clase funcione de manera correcta se ha establecido un
+	 * atributo tridimensional en la clase map que contiene 7 matrices que acaparan
+	 * el mapa.
+	 */
+
+	// ATRIBUTOS DEL TILE.
+
+	private Image imagen;
+	private boolean colision;
 	
-	/* Está clase dentro del paquete de los modelos se encargara de modelar la estructura del modelo mapa  mediante aspectos gráficos
-	 * para ello se utilizaran los distintos sprites  para las platoformas pinchos y coleccionables.
-	 * a su vez  de que para que está clase funcione de manera correcta se ha establecido  un atributo tridimensional en  la clase map
-	 * que contiene 7 matrices que acaparan el mapa.*/
+	//CONSTRUCTOR
+	public Tile(Image imagen,boolean colision) {
+		this.imagen=imagen;
+		this.colision=colision;
+		
+	}
 
 
+	
+	// GETTERS Y SETTERS.
+	public Image getImagen() {
+		return imagen;
+	}
 
 
+	public void setImagen(Image imagen) {
+		this.imagen = imagen;
+	}
 
 
+	public boolean isColision() {
+		return colision;
+	}
 
 
+	public void setColision(boolean colision) {
+		this.colision = colision;
+	}
 
-
-
+	
+	
 }
