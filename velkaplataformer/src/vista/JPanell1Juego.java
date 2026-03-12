9c80b82e47cc2db8c7c60b5a724b0505fe36c665
Miguel <miguelbedmards@gmail.com>
2026-03-12 12:39:48 +0100

    Se ha comenzado a trabajar en el Jpanel Juego

--- velkaplataformer/src/vista/JPanell1Juego.java
+++ velkaplataformer/src/vista/JPanell1Juego.java
@@ -4,9 +4,17 @@ import modelo.Map;
 import modelo.Tile;
 
 public class JPanell1Juego {
+	// Atributos
 	private Map mapaModelo;
-	private Tile []tile;
+	private Tile[] tile;
+
+	// Constructor
+	public JPanell1Juego() {
+		mapaModelo = new Map();
+		tile = new Tile[5];
+	}
+
 	
-	
+	//Metodos
 	
 }
