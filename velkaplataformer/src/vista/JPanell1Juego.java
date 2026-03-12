1d13e4324025950128d9bd248d8b3c4c8ee2de09
Miguel Bedmar <miguelbedmards@gmail.com>
2026-03-12 19:49:23 +0100

    Se ha comenzado a modificar el mapa

--- velkaplataformer/src/vista/JPanell1Juego.java
+++ velkaplataformer/src/vista/JPanell1Juego.java
@@ -12,15 +12,15 @@ import modelo.Tile;
 
 public class JPanell1Juego extends JPanel {
 	// Atributos
-	private Map  mapaModelo;
+	private Map mapaModelo;
 	private Tile[] tile;
 
 	// Constructor
 	public JPanell1Juego() throws IOException {
 		mapaModelo = new Map();
-		tile = new Tile[6];
+		tile = new Tile[9];
 		asignarTiles();
-		
+
 	}
 
 	// Metodos
@@ -28,32 +28,37 @@ public class JPanell1Juego extends JPanel {
 	/*
 	 * EL metodo asignarTiles Este metodo se encarga de asignar las colisiones que
 	 * en función del número será una colison o no tendrá colisión Empezamos con
-	 * declarar un objeto de tipo ImageIO que se guardara en un objeto de tipo 
-	 * BufferredImagen posteriormente se le asigna   imagen y decir si tendra colision o no
-	 * la colision asignada unicamente se indica con false o true
+	 * declarar un objeto de tipo ImageIO que se guardara en un objeto de tipo
+	 * BufferredImagen posteriormente se le asigna imagen y decir si tendra colision
+	 * o no la colision asignada unicamente se indica con false o true
 	 */
 	public void asignarTiles() throws IOException {
-		
+
 		tile[0] = new Tile(null, false);
 		BufferedImage plataforma = ImageIO.read(getClass().getResource("/recursos/recursosSPRITESMAPA/PLATAFORMA.png"));
 		tile[1] = new Tile(plataforma, true);
 		BufferedImage pincho = ImageIO.read(getClass().getResource("/recursos/recursosSPRITESMAPA/PINCHOS.png"));
 		tile[2] = new Tile(pincho, true);
-		BufferedImage Coleccionable = ImageIO.read(getClass().getResource("/recursos/recursosSPRITESMAPA/ESTRELLAS.png"));
+		BufferedImage Coleccionable = ImageIO
+				.read(getClass().getResource("/recursos/recursosSPRITESMAPA/ESTRELLAS.png"));
 		tile[3] = new Tile(Coleccionable, true);
-		BufferedImage Colecciona = ImageIO.read(getClass().getResource("/recursos/recursosSPRITESMAPA/ESTRELLAS.png"));
-		tile[4] = new Tile(plataforma, true);
-		BufferedImage PORTAL = ImageIO.read(getClass().getResource("/recursos/recursosSPRITESMAPA/PORTAL.png"));
-		tile[5] = new Tile(PORTAL, true);
+		tile[4] = new Tile(null, false);
+		BufferedImage portal = ImageIO.read(getClass().getResource("/recursos/recursosSPRITESMAPA/PORTAL.png"));
+		tile[5] = new Tile(portal, true);
+		BufferedImage PILAR = ImageIO.read(getClass().getResource("/recursos/recursosSPRITESMAPA/PILAR.png"));
+		tile[6] = new Tile(PILAR, false);
+		BufferedImage puerta = ImageIO.read(getClass().getResource("/recursos/recursosSPRITESMAPA/PORTAL.png"));
+
+		tile[7] = new Tile(pincho, false);
 	}
 
 	@Override
 	public void paint(Graphics g) {
-		
-		for(int i =0;i<mapaModelo.getRoomActual();i++) {
-			
+
+		for (int i = 0; i < mapaModelo.getRoomActual(); i++) {
+
 		}
-		
+
 	}
 
 }
