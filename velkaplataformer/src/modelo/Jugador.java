5e67191f3a8c2b05eab55b7446b4632b78b8ea34
Miguel <miguelbedmards@gmail.com>
2026-03-13 23:38:40 +0100

    Se ha  terminado el constructor de jugador y se ha comenzado a asignar los sprites

--- velkaplataformer/src/modelo/Jugador.java
+++ velkaplataformer/src/modelo/Jugador.java
@@ -18,14 +18,23 @@ public class Jugador extends Personaje implements Runnable {
 	private int spriteActual;
 	private int contadorFra;
 	private String posturaActual;
+	private Thread hiloJugador;
 
-	
-	
 	// CONSTRUCTOR JUGADOR.
 	public Jugador() {
+		vida = 3;
+		velocidad = 4;
+		suelo = false;
+
+		// INICIALIZACION DE LOS SPRITES
+		Idle = new BufferedImage[6];
+		correr = new BufferedImage[4];
+		salto = new BufferedImage[3];
+	}
+
+	private void asignarsprites() {
 		
 	}
-	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
