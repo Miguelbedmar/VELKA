b1a28b673047dcda4d45d290bac7e6f6a2c1d93a
Miguel Bedmar <miguelbedmards@gmail.com>
2026-03-13 18:57:04 +0100

    Se han añadido los atributos a jugador

--- velkaplataformer/src/modelo/Jugador.java
+++ velkaplataformer/src/modelo/Jugador.java
@@ -1,28 +1,40 @@
 package modelo;
 
-public class Jugador extends Personaje implements Runnable  {
-	
+import java.awt.image.BufferedImage;
+
+public class Jugador extends Personaje implements Runnable {
+
 	// ATRIBUTOS DEL JUGADOR
 	private int vida;
-	
-	
+	private int velocidad;
+	private boolean suelo;
+
+	// ATRIBUTOS ANIMACIONES.
+
+	private BufferedImage[] Idle;
+	private BufferedImage[] correr;
+	private BufferedImage[] salto;
+
+	private int spriteActual;
+	private int contadorFra;
+	private String posturaActual;
 
 	
 	
-	
+	// CONSTRUCTOR JUGADOR.
+	public Jugador() {
+		
+	}
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub	
+		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public void gravedad() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
-	
-	
 }
