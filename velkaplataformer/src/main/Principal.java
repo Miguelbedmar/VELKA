330bd5c30d4b0938177df72ca20bfcebbb989cef
Miguel Bedmar <miguelbedmards@gmail.com>
2026-03-16 18:00:59 +0100

    Se ha implementado la musica de manera exitosa

--- velkaplataformer/src/main/Principal.java
+++ velkaplataformer/src/main/Principal.java
@@ -1,10 +1,15 @@
 package main;
 
+import java.io.IOException;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 import vista.JFramePrincipal;
 
 public class Principal {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
 		JFramePrincipal ventana = new JFramePrincipal();
 		ventana.setVisible(true);
 	}
--- velkaplataformer/src/vista/JFramePrincipal.java
+++ velkaplataformer/src/vista/JFramePrincipal.java
@@ -2,16 +2,21 @@ package vista;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.io.IOException;
 
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
 
 import javax.swing.JFrame;
 
-public class JFramePrincipal extends JFrame {
+import modelo.ReproductoMusica;
 
+public class JFramePrincipal extends JFrame {
+	private ReproductoMusica music;
 	JpanelMenu panel = new JpanelMenu();
-	
-	public JFramePrincipal() {
+
+	public JFramePrincipal() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
 		setTitle("VELKA");
 		setSize(new Dimension(800, 800));
 		setLocationRelativeTo(null);
@@ -20,10 +25,9 @@ public class JFramePrincipal extends JFrame {
 		add(panel, BorderLayout.CENTER);
 		ImageIcon icono = new ImageIcon(getClass().getResource("/recursos/VELKAAA.png"));
 		setIconImage(icono.getImage());
+		music=new ReproductoMusica("/recursos/MUSICA/MusicaMenu.wav");
+		music.reproducirAudio();
 		
-	
 	}
-	
-	
 
 }
